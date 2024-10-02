b26ce8e76c95a4a662f8e4f686b29140ff6a13eb
Beginner10617 <wasihusain23@iitk.ac.in>
2024-10-02 22:50:35 +0530

    health added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	public float maxHealth = 100;
+	public float currentHealth;
+
+	public HealthBar healthBar;
+	public float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		currentHealth = maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		TakeDamage(damage*Time.deltaTime);
+		if(currentHealth==0)
+		{
+			Application.Quit(0);
+		}
+    }
+
+	void TakeDamage(float damage)
+	{
+		currentHealth -= damage;
+
+		healthBar.SetHealth(currentHealth);
+	}
+}
